f6b02ed165bf828f1f68753674c0177aae6045b5
Tomas Radvansky <tomas@rdev.co.nz>
2024-03-26 17:53:50 +1300

    !feat: add emulators check

--- packages/rdev_riverpod_versioning/lib/data/shared_providers.dart
+++ packages/rdev_riverpod_versioning/lib/data/shared_providers.dart
@@ -1,7 +1,9 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logging/logging.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:rdev_riverpod_versioning/domain/fireway_vo.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 import '../application/fireway_service.dart';
 import '../domain/flavor_enum.dart';
@@ -16,6 +18,18 @@ final packageInfoProvider = FutureProvider<PackageInfo>((ref) {
     log.severe('Getting packageInfo failed.', err);
     rethrow;
   }
+});
+
+final isEmulatorProvider = FutureProvider<bool>((ref) async {
+  final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  if (UniversalPlatform.isAndroid) {
+    var build = await deviceInfoPlugin.androidInfo;
+    return build.isPhysicalDevice == false;
+  } else if (UniversalPlatform.isIOS) {
+    var data = await deviceInfoPlugin.iosInfo;
+    return data.isPhysicalDevice == false;
+  }
+  return false;
 });
 
 final firewayProvider = FutureProvider<FirewayVO>((ref) {
